65d8387ae36937800bd364ef719717cc2509e9cd
QdKyi <denroy777@gmail.com>
2020-11-12 19:02:06 +0200

    readme + last changes

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # AlgoLab3
+
+## Algorithm Description
+My function takes number of pairs and list of this pairs,
+(sorting pair list before processing to the next step)
+after this for each of pairs i'm looping through tribes list
+and creates new tribe if this pair have none or add this pair
+to existing tribe. After this dividing all pairs into tribes
+i'm calculating number of female male pairs from different tribes
+
+## Setup
+if you want to run tests using cmd
+```commandline
+py main.py
+```
+or
+```commandline
+python3 main.py
+```
--- main.py
+++ main.py
@@ -6,12 +6,18 @@ def wedd(N, people_list):
     """
     >>> test_list = [(1, 2), (2, 4), (3, 5), (7, 8), (2, 15), (10, 8), (15, 20)]
     >>> another_test_list = [(2, 1), (4, 2), (3, 5), (7, 8), (2, 15), (10, 8), (15, 20), (5, 25), (10, 55)]
+    >>> first_example_list = [(1, 2), (2, 4), (3, 5)]
+    >>> second_example_list = [(1, 2), (2, 4), (1, 3), (3, 5), (8, 10)]
     >>> wedd(len(test_list), test_list)
     17
     >>> wedd(len(another_test_list), another_test_list)
     25
+    >>> wedd(len(first_example_list), first_example_list)
+    4
+    >>> wedd(len(second_example_list), second_example_list)
+    6
     """
-
+    people_list.sort(key=lambda x: x)
     tribes = []
     for people in people_list:
 
@@ -36,7 +42,7 @@ def wedd(N, people_list):
 
 
 if __name__ == '__main__':
-    sample_list = [(1, 2), (2, 4), (3, 5), (7, 8), (2, 15), (10, 8), (15, 20)]
+    sample_list = [(1, 2), (2, 4), (3, 5), (2, 3), (2, 15), (10, 8), (15, 20)]
 
     N = 7
     print(sample_list)
@@ -44,4 +50,3 @@ if __name__ == '__main__':
     print(res)
 
     doctest.testmod(verbose=True)
-
